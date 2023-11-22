5cc1f36520a906085231414456f2509849c4e960
Saint <sainthill1996@gmail.com>
2023-11-22 13:40:15 +0100

    Commit for DGmarkProjectNew Updated

---
+++ src/test/java/com/dgmarkt/pages/EditPasswordPage.java
@@ -0,0 +1,34 @@
+package com.dgmarkt.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditPasswordPage {
+
+    @FindBy(xpath = "//a[@title='My Account']/..")
+    public WebElement myAccountBtn;
+
+    @FindBy(xpath = "(//a[text()='My Account'])[1]")
+    public WebElement myAccountBtnUnderMyAccount;
+    @FindBy(xpath = "//a[text()='Change your password']")
+    public WebElement changeYourPasswordLink;
+
+    @FindBy(xpath = "//*[@id='input-password']")
+    public WebElement inPutPassword;
+
+    @FindBy(xpath = "//*[@id='input-confirm']")
+    public WebElement passwordConfirm;
+
+    @FindBy(xpath = "//*[@value='Continue']")
+    public WebElement continueBtn;
+
+    @FindBy(xpath = "//*[@id='account-account']/div[1]")
+    public WebElement successMsg;
+
+    @FindBy(xpath = "//div[text()='Password confirmation does not match password!']")
+    public WebElement error_MsgText1;
+
+    @FindBy(xpath = "//*[@id='content']/form/fieldset/div[2]/div/div")
+    public WebElement error_MsgText2;
+}
+

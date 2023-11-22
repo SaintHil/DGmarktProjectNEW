bb4421e6fec7d3dee235abd9c6e3d8c03b447515
Ayse3434 <aysegul2013x@gmail.com>
2023-11-22 21:32:13 +0100

    currency 1

--- src/test/java/com/dgmarkt/stepDefinitions/CurrencyStepDefinitions.java
+++ src/test/java/com/dgmarkt/stepDefinitions/CurrencyStepDefinitions.java
@@ -1,6 +1,122 @@
 package com.dgmarkt.stepDefinitions;
 
+import com.dgmarkt.pages.CurrencyPage;
+import com.dgmarkt.pages.LoginPage;
+import com.dgmarkt.utilities.BrowserUtils;
+import com.dgmarkt.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
 public class CurrencyStepDefinitions {
+    CurrencyPage currencyPage = new CurrencyPage();
+    String expCurrency;
+    LoginPage loginPage = new LoginPage();
+    @When("The user clicks Checkout button")
+    public void the_user_clicks_checkout_button() {
+        currencyPage.checkOut_btn.click();
+    }
+
+    @When("The user clicks continue for billing details with exist credentials")
+    public void the_user_clicks_continue_for_billing_details_with_exist_credentials() {
+        currencyPage.billingContinue_btn.click();
+
+    }
+
+    @When("The user clicks continue for delivery details with exist credentials")
+    public void the_user_clicks_continue_for_delivery_details_with_exist_credentials() {
+        currencyPage.deliveryContinue_btn.click();
+
+    }
+
+    @When("The user adds comment about his order {string} and clicks continue for delivery method")
+    public void the_user_adds_comment_about_his_order_and_clicks_continue_for_delivery_method(String comment) {
+        currencyPage.deliveryMethodCommentArea_text.sendKeys(comment);
+        currencyPage.deliveryMethodContinue_btn.click();
+    }
+
+    @When("The user adds comment about his order {string} and clicks checkbox to agree Terms and Conditions and clicks continue")
+    public void the_user_adds_comment_about_his_order_and_clicks_checkbox_to_agree_terms_and_conditions_and_clicks_continue(String comment) {
+        currencyPage.deliveryMethodCommentArea_text.sendKeys(comment);
+        BrowserUtils.waitForClickablility(currencyPage.agreeTermsAndConditions_checkBox, 3);
+        currencyPage.agreeTermsAndConditions_checkBox.click();
+        currencyPage.paymentContinue_btn.click();
+    }
+
+    @Then("Verify that confirm order total price is in expected currency {string}")
+    public void verify_that_confirm_order_total_price_is_in_expected_currency(String expCurrecyIcon) {
+        BrowserUtils.waitFor(2);
+        String actTotalPrice = currencyPage.confirmOrderTotalPrice_text.getText();
+        Assert.assertTrue(actTotalPrice.contains(expCurrecyIcon));
+        Driver.get().navigate().back();
+        currencyPage.removeAllProductfromCart();
+    }
 
 
+    @When("The user clicks currency button")
+    public void the_user_clicks_currency_button() {
+        currencyPage.currency_btn.click();
+    }
+
+    @Then("The user click the {string} Button")
+    public void the_user_click_the_button(String currencyName) {
+        expCurrency = currencyPage.currencySubmenuByName_mtd(currencyName).getText();
+        currencyPage.currencySubmenuByName_mtd(currencyName).click();
+
+    }
+
+    @Then("Verify that the user sees select currency")
+    public void verify_that_the_user_sees_select_currency() {
+        Assert.assertTrue(expCurrency.contains(currencyPage.currentCurrency_text.getText()));
+
+    }
+
+    @Then("The user closes cookies pop up")
+    public void the_user_closes_cookies_pop_up() {
+        loginPage.popUpClose_Btn.click();
+
+    }
+
+    @When("The user navigates to sub-category {string}")
+    public void the_user_navigates_to_sub_category(String subCategoryname) {
+        BrowserUtils.waitFor(2);
+        currencyPage.goToSubCategoryByName_mtd(subCategoryname);
+
+    }
+
+
+    @When("The user clicks cart icon button")
+    public void the_user_clicks_cart_icon_button() {
+        currencyPage.cartIcon_btn.click();
+
+
+    }
+
+    @When("The user clicks view cart button")
+    public void the_user_clicks_view_cart_button() {
+        BrowserUtils.clickWithJS(currencyPage.viewCart_btn);
+
+    }
+
+    @When("The user adds  products to the cart list names {string}")
+    public void the_user_adds_products_to_the_cart_list_names(String productsname) {
+        currencyPage.productSelect(productsname);
+
+    }
+
+    @Then("Verify that total price is in expected currency {string}")
+    public void verify_that_total_price_is_in_expected_currency(String expectedCurrency) {
+        String actTotalPrice = currencyPage.totalPrice.getText();
+        Assert.assertTrue(actTotalPrice.contains(expectedCurrency));
+        currencyPage.removeAllProductfromCart();
+    }
+
+
+    @And("The user adds two products to the cart list names {string} and {string}")
+    public void theUserAddsTwoProductsToTheCartListNamesAnd(String item1, String item2) {
+        BrowserUtils.waitFor(2);
+        currencyPage.addToCartByProductName_mtd(item1);
+        currencyPage.addToCartByProductName_mtd(item2);
+    }
 }
--- src/test/resources/features/currency.feature
+++ src/test/resources/features/currency.feature
@@ -1,3 +1,42 @@
-
+@D2DGMRT-4
 Feature: Currency Functionality
 
+
+  Background: The user logs in for login panel
+    Given The user navigate to web page
+    And The user enters login panel credential and clicks login button
+    And The user closes cookies page
+    And The user clicks the login under the my account
+    And The user enters account credentials "aysegun@gmail.com" and "Test_1234" and clicks login
+
+
+  @D2DGMRT-48
+  Scenario Outline: The user sees the currency selected
+    When The user clicks currency button
+    And The user click the "<currencyName>" Button
+    And The user closes cookies pop up
+    Then Verify that the user sees select currency
+    Examples:
+      | currencyName   |
+      | Euro           |
+      | Pound Sterling |
+      | US Dollar      |
+  @D2DGMRT-69
+  Scenario Outline: The user completes his/her purchase with the currency his/her choice
+    When The user navigates to sub-category "Networking"
+    And The user adds two products to the cart list names "ASUS ROG STRIX GS-AX3000" and "Belkin Secure Flip KVM Switch"
+    And The user clicks cart icon button
+    And The user clicks view cart button
+    And The user clicks currency button
+    And The user click the "<currencyName>" Button
+    And The user clicks Checkout button
+    And The user clicks continue for billing details with exist credentials
+    And The user clicks continue for delivery details with exist credentials
+    And The user adds comment about his order "Everything is ok!" and clicks continue for delivery method
+    And The user adds comment about his order "Thanks! Everything is ok!" and clicks checkbox to agree Terms and Conditions and clicks continue
+    Then Verify that confirm order total price is in expected currency "<expectedicon>"
+    Examples:
+      | currencyName   | expectedicon |
+      | Euro           | €            |
+      | Pound Sterling | £            |
+      | US Dollar      | $            |

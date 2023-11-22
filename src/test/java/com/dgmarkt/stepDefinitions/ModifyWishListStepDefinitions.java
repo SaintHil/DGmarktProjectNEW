ccb9a5268c03dacd8af142d895708a4f1cc62ccb
ocolak95 <ocolak95@hotmail.com>
2023-11-22 18:21:34 +0100

    US11-US13 ready 4

--- src/test/java/com/dgmarkt/stepDefinitions/ModifyWishListStepDefinitions.java
+++ src/test/java/com/dgmarkt/stepDefinitions/ModifyWishListStepDefinitions.java
@@ -1,9 +1,6 @@
 package com.dgmarkt.stepDefinitions;
 
-import com.dgmarkt.pages.BasePage;
-import com.dgmarkt.pages.HomePage;
-import com.dgmarkt.pages.LoginPage;
-import com.dgmarkt.pages.WishListPage;
+import com.dgmarkt.pages.*;
 import com.dgmarkt.utilities.BrowserUtils;
 import com.dgmarkt.utilities.ConfigurationReader;
 import com.dgmarkt.utilities.Driver;
@@ -16,10 +13,11 @@ import org.junit.Assert;
 public class ModifyWishListStepDefinitions {
 
     WishListPage wishListPage = new WishListPage();
+    EditPasswordPage editPasswordPage=new EditPasswordPage();
     @When("The user clicks to My Account button under My Account submenu")
     public void the_user_clicks_to_my_account_button_under_my_account() {
         wishListPage.myAccount_Btn.click();
-        wishListPage.subMyAccountBtn.click();
+        editPasswordPage.myAccountBtnUnderMyAccount.click();
     }
     @When("The user clicks to Wish List button on the Account page")
     public void the_user_clicks_to_wish_list_button_on_the_account_page() {
@@ -29,20 +27,5 @@ public class ModifyWishListStepDefinitions {
     public void verify_that_the_user_sees_the_my_wish_list() {
         Assert.assertTrue(wishListPage.myWishListPageText.isDisplayed());
     }
-    @And("The user clicks to add product button")
-    public void the_user_clicks_to_add_product_button() {
-        wishListPage.AddToCartBtn.click();
-    }
-    @Then("Verify that the user sees success message")
-    public void verify_that_the_user_sees_success_message() {
-        Assert.assertTrue(wishListPage.successMsgAddToCart.getText().contains("Success"));
-    }
-    @And("The user clicks to delete product button")
-    public void the_user_clicks_to_delete_product_button() {
-        wishListPage.remove_Btn.click();
-    }
-    @Then("Verify that the user sees successfully modified message")
-    public void verify_that_the_user_sees_successfully_modified_message() {
-        Assert.assertTrue(wishListPage.successMsg.getText().contains("Success: You have modified"));
-    }
+
 }
